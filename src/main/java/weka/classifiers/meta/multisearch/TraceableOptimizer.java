30c9279337fbdb7cf2f3d0d93b985b027a4c2cdc
Peter Reutemann <fracpete@gmail.com>
2016-05-15 18:36:13 +1200

    added GPL preamble and copyright; fixed javadoc

--- src/main/java/weka/classifiers/meta/multisearch/TraceableOptimizer.java
+++ src/main/java/weka/classifiers/meta/multisearch/TraceableOptimizer.java
@@ -1,3 +1,23 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * TraceableOptimizer.java
+ * Copyright (C) 2016 Leiden University, NL
+ */
+
 package weka.classifiers.meta.multisearch;
 
 import java.util.List;
@@ -5,38 +25,42 @@ import java.util.Map.Entry;
 
 public interface TraceableOptimizer {
 
-	/**
-	 * Returns the size of m_Trace, which is technically the amount of setups
-	 * that where tested in order to find the best.
-	 */
-	public int getTraceSize();
+  /**
+   * Returns the size of m_Trace, which is technically the amount of setups
+   * that where tested in order to find the best.
+   *
+   * @return		the number of setups tested
+   */
+  public int getTraceSize();
 
-	/**
-	 * Returns the CLI string of a given item in the trace.
-	 * 
-	 * @param index
-	 *            the index of the trace item to obtain
-	 */
-	public String getTraceClassifierAsCli(int index);
+  /**
+   * Returns the CLI string of a given item in the trace.
+   *
+   * @param index	the index of the trace item to obtain
+   * @return		the commandline
+   */
+  public String getTraceClassifierAsCli(int index);
 
-	/**
-	 * Returns the performance score of a given item in the trace.
-	 * 
-	 * @param index
-	 *            the index of the trace item to obtain
-	 */
-	public Double getTraceValue(int index);
+  /**
+   * Returns the performance score of a given item in the trace.
+   *
+   * @param index	the index of the trace item to obtain
+   * @return		the trace value
+   */
+  public Double getTraceValue(int index);
 
-	/**
-	 * Returns the folds of a given item in the trace.
-	 *
-	 * @param index
-	 *            the index of the trace item to obtain
-	 */
-	public Integer getTraceFolds(int index);
+  /**
+   * Returns the folds of a given item in the trace.
+   *
+   * @param index	the index of the trace item to obtain
+   * @reutrn		the number of folds
+   */
+  public Integer getTraceFolds(int index);
 
-	/**
-	 * Returns the full trace.
-	 */
-	public List<Entry<Integer, Performance>> getTrace();
+  /**
+   * Returns the full trace.
+   *
+   * @return		the full trace
+   */
+  public List<Entry<Integer, Performance>> getTrace();
 }
